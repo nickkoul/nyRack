1ace63e39954ab8bc87e1d2585a9b40e7c272065
nickkoul <nickkoul13@gmail.com>
2016-04-02 17:04:30 -0400

    parse input file

--- main.py
+++ main.py
@@ -2,3 +2,30 @@ import node
 
 if __name__ == '__main__':
     print "nyRack"
+
+    f = open("map","r") # make this the Input File
+
+    new_nodes=[]
+    i=0
+    for line in f:
+        line = line.strip()
+
+        if len(line)>5 and str(line[1:5]) == "node": # actual node line
+            lat=0
+            lon =0
+            # need to deal with when id increases by a digit
+            if str(line[20:23]) == "lat":
+                lat = float(line[25:35])
+                lon = float(line[42:53])
+            elif str(line[21:24]) == "lat":
+                lat = float(line[26:36])
+                lon = float(line[43:53])
+            elif str(line[22:25]) == "lat":
+                lat = float(line[27:37])
+                lon = float(line[44:54])
+            else:
+                print"ERROR IN READING FILE"
+            loc = (lat,lon)
+            new_nodes.append(node.Node(location=loc,does_exist=False))
+
+        i+=1
