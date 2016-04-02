218c629ccc549379b24a70ff73c7049acfdb4e2e
Raymond Jacobson <me@raymondjacobson.com>
2016-04-02 19:03:15 -0400

    make map frontend

--- api.py
+++ api.py
@@ -1,19 +1,37 @@
-from flask import Flask, jsonify
+import main, json
+from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('map.html')
 
 @app.route('/citibike', methods=['GET'])
 def get_citibike():
-    return jsonify({'data': 5})
+    citi_nodes = main.get_citiBike_stations()
+    citi_nodes = list(map(lambda node: (node.location[1],
+                                        node.location[0]), citi_nodes))
+    return json.dumps(citi_nodes)
 
-@app.route('/possible', methods=['GET'])
-def get_possible():
-    return jsonify({'data': 5})
+@app.route('/new', methods=['GET'])
+def get_new():
+    new_nodes = main.read_new_nodes()
+    new_nodes = list(map(lambda node: (node.location[1],
+                                       node.location[0]), new_nodes))
+    return json.dumps(new_nodes)
+
+@app.route('/existing', methods=['GET'])
+def get_existing():
+    existing_nodes = main.read_exisiting_nodes()
+    existing_nodes = list(map(lambda node: (node.location[1],
+                                            node.location[0]), existing_nodes))
+    return json.dumps(existing_nodes)
 
 @app.route('/selections', methods=['GET'])
 def get_selections():
     k = request.args.get('k')
-    return jsonify({'data': 5})
+    return json.dumps({'data': 5})
 
 if __name__ == '__main__':
     app.run(debug=True)
